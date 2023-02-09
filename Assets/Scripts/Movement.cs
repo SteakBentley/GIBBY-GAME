9c82ee75c86258e060810e4883591e324cb07f3b
SteakBentley <steakbentley@gmail.com>
2023-02-09 00:37:36 -0500

    Changed model, added stock materials

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    //Movement
+    [SerializeField] private int speed = 5;
+    [SerializeField] private Vector3 moveDirection;
+    [SerializeField] private CharacterController controller;
+    //Animation
+    private Animator animator;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+
+        //Get horizontal and vertical inputs (usually WASD or Arrow Keys)
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(horizontal, 0, vertical);
+        moveDirection *= speed;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //animation
+        if (moveDirection == Vector3.zero)
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+        else
+            animator.SetFloat("Speed", 1f);
+    }
+}
